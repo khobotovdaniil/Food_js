bf8bf729f6e09ef28e7932fd3431e2ae58c04155
khobotovdaniil <dinich44@gmail.com>
2022-05-20 18:08:00 +0300

    add RegExp

--- js/script.js
+++ js/script.js
@@ -336,16 +336,19 @@ window.addEventListener('DOMContentLoaded', () => {
         dots[slideindex - 1].style.opacity = 1;
     };
 
-
     const transformSlideField = (x) => {
         slidesField.style.transform = `translateX(-${x}px)`;
     };
 
+    const deleteNotDigits = str => {
+        return +str.replace(/\D/g, '');
+    };
+
     nextSlide.addEventListener('click', () => {
-        if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+        if (offset == deleteNotDigits(width) * (slides.length - 1)) {
             offset = 0;
         } else {
-            offset += +width.slice(0, width.length - 2);
+            offset += deleteNotDigits(width);
         }
 
         transformSlideField(offset);
@@ -362,9 +365,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
     prevSlide.addEventListener('click', () => {
         if (offset == 0) {
-            offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+            offset = deleteNotDigits(width) * (slides.length - 1);
         } else {
-            offset -= +width.slice(0, width.length - 2);
+            offset -= deleteNotDigits(width);
         }
 
         transformSlideField(offset);
@@ -384,7 +387,7 @@ window.addEventListener('DOMContentLoaded', () => {
             const slideTo = e.target.getAttribute('data-slide-to');
 
             slideindex = slideTo;
-            offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+            offset = deleteNotDigits(width) * (slideTo - 1);
 
             transformSlideField(offset);
 
